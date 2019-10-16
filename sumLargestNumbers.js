233d96dba103de3a93ebe172b673a47166762c4f
Jay Chai <chunsaa@gmail.com>
2019-10-16 15:14:44 +0000

    Fixed eslint formatting

--- sumLargestNumbers.js
+++ sumLargestNumbers.js
@@ -1,15 +1,14 @@
 let sumLargestNumbers = function(data) {
-  if (data.length == 0){
+  if (data.length === 0) {
     return "No Data";
-  }
-  else if (data.length == 1){
+  } else if (data.length === 1) {
     return data[0];
-  }
-  else if (data.length == 2){
+  } else if (data.length === 2) {
     return data[0] + data[1];
-  }
-  else{    
-    data.sort(function (a, b){return a - b}); 
+  } else {
+    data.sort(function(a, b) {
+      return a - b;
+    });
     return data[data.length - 1] + data[data.length - 2];
   }
 };
